150e1e5c3493a0c45b2c5f26480e29ee4ac3e38b
xiaobaowan1988 <baoliangwan1988@gmail.com>
2021-04-28 15:30:02 +0800

    add api

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
- <amplify-authenticator>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App v2"/>
-  <amplify-sign-out></amplify-sign-out>
- </amplify-authenticator>
+  <div id="app">
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name">
+    <input type="text" v-model="description" placeholder="Todo description">
+    <button v-on:click="createTodo">Create Todo</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  async created() {
+    this.getTodos();
+    this.subscribe();
+  },
+  data() {
+    return {
+      name: '',
+      description: '',
+      todos: []
+    }
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      this.todos = [...this.todos, todo];
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo},
+      });
+      this.name = '';
+      this.description = '';
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      });
+      this.todos = todos.data.listTodos.items;
+    },
+    async subscribe() {
+      await API.graphql({ query: onCreateTodo })
+        .subscribe({
+          next: (eventData) => {
+            let todo = eventData.value.data.onCreateTodo;
+            if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
+            this.todos = [...this.todos, todo];
+          }
+        });
+    }
+  }
+};    
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
